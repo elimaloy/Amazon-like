07ad9264343362bf05638533f3c62b6393154901
Lizzie <elimaloy@gmail.com>
2019-10-02 17:52:14 -0700

    bamazon project

---
+++ bamazonCustomer.js
@@ -0,0 +1,120 @@
+var mysql      = require('mysql');
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+  host     : "localhost",
+  user     : "root",
+  password : "password",
+  database : "bamazon",
+});
+ 
+connection.connect(function(err){
+	if(err){
+		throw err;
+	}
+	showItems();
+});
+
+function showItems(){
+
+	var sql = "SELECT item_id, product_name, price FROM products";
+	connection.query(sql, function(err,result){
+		if(err){
+			throw err;
+		}
+		var table = new Table({
+			head: ['Item Id','Product Name', 'Price']
+		})
+		for (var i = 0; i < result.length; i++) {
+			table.push(
+				[result[i].item_id , result[i].product_name , result[i].price]
+			);
+		}
+		console.log(table.toString());
+		placeOrder();
+	})
+	
+
+}
+
+function placeOrder(){
+	inquirer
+    .prompt([
+	    {
+	        name: "itemId",
+	        type: "input",
+	        message: "What is the Id of the item that you would like to buy?"
+	    },
+	    {
+	        name: "units",
+	        type: "input",
+	        message: "\n How many units of the product you would like to buy?",
+	        validate: function(value){
+	        	if(isNaN(value) === false){
+	        		return true;
+	        	}
+	        	return false;
+	        }
+	    }
+    ])
+    .then(function(answer) {
+    	var userItem = answer.itemId;
+    	var userQuantity = answer.units;
+    	var sql1 = "SELECT * FROM products WHERE ?";
+    	connection.query(sql1,{item_id: userItem},function(err, data){
+    		if(err) throw err;
+    		var container = data[0];
+    		if(data.length == 0){
+    			console.log(" \n Please select a valid Item Id  ");
+    			showItems();
+    		}
+    		else{
+    			if(userQuantity <= data[0].stock_quantity){
+    				console.log("\n Your Requested Item is in Stock  "+"\n");
+    				connection.query(
+    					"UPDATE products SET ? WHERE ? ",
+    					//[
+    						{
+    							stock_quantity: data[0].stock_quantity - userQuantity,
+    							product_sales: data[0].product_sales + data[0].price*userQuantity
+    						},
+    						{
+    							item_id: userItem
+    						}
+    					//],
+    					,function(err,data){
+						1
+							if(err) throw err;
+	    					console.log("  YOUR ORDER IS PLACED  \n");
+	    					console.log("  YOUR TOTAL AMOUNT IS $" + container.price*userQuantity +"  \n");
+	    					inquirer
+    						.prompt([
+	    						{
+							        name: "confirm",
+							        type: "confirm",
+							        message: "Do you want to shop again?"
+								}
+	    					])
+	    					//connection.end();
+	    					.then(function(answer) {
+	    						if(answer.confirm === true){
+
+	    							showItems();
+	    						}
+	    						else{
+	    							connection.end();
+	    						}
+	    					});
+    					}
+    				);
+    			}
+    			else{
+    				console.log("\nSorry, We are out of Stock");
+    				showItems();
+    			}
+    		}
+
+    	})
+    });
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+
+
+ The app asks the user two questions. 
+ What is the the ID of the product they would like to buy?
+ How many units of the product they would like to buy?
+
+ The customer then places an order and it will check if your store product.
+
+ If not not enough product the customer will recieve a message "Insufficient quantity!", then no order will be placed.
+
+ If there is enough quintity the order will be fullfilled.
+
+https://gph.is/g/ZP7lvx8
+short link
+
+https://media.giphy.com/media/RjldeaRzJUMFN4Gtsl/giphy.gif
+link to gif image
+
+https://giphy.com/gifs/RjldeaRzJUMFN4Gtsl/html5
+video that loads a faster version
